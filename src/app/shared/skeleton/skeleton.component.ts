3cc475799c295a21e376f33d273f87e0afe3bf5e
Cida Luna <cidalunna@hotmail.com>
2026-02-22 14:49:54 -0300

    feat: adjust scss

--- src/app/shared/skeleton/skeleton.component.ts
+++ src/app/shared/skeleton/skeleton.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { SkeletonType } from './skeleton.types';
 import { timer } from 'rxjs';
 import { CommonModule } from '@angular/common';

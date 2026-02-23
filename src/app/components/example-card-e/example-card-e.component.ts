286be8511d39fad44ca2ae08932966cf41abc3e0
Cida Luna <cidalunna@hotmail.com>
2026-02-23 20:47:21 -0300

    feat: refactor skeletons

--- src/app/components/example-card-e/example-card-e.component.ts
+++ src/app/components/example-card-e/example-card-e.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal, ChangeDetectionStrategy } from '@angular/core';
+import { SkeletonComponent } from '../../shared/skeleton/skeleton.component';
 
 @Component({
   selector: 'app-example-card-e',
   standalone: true,
-  imports: [],
+  imports: [SkeletonComponent],
   templateUrl: './example-card-e.component.html',
-  styleUrl: './example-card-e.component.scss'
+  styleUrl: './example-card-e.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExampleCardEComponent {
+  @Input() mainTitle!: string;
+  @Input() mainDescription!: string;
+  showSkeletonTwo = signal(false);
 
+  // controle simples para evitar cliques múltiplos
+  isProcessing = signal(false);
+  totalCards: number = 5;
+
+  callSkeletonTwo() {
+      //this.showSkeletonTwo.update(v => !v);
+      //se já estiver processando, ignora o clique
+      if (this.isProcessing()) return;
+
+      this.isProcessing.set(true);
+      this.showSkeletonTwo.set(true);
+
+      // mostra por 4s e depois oculta
+      setTimeout(() => {
+        this.showSkeletonTwo.set(false);
+        this.isProcessing.set(false);
+      }, 4000);
+    }
 }
--- src/app/components/host-card/host-card.component.ts
+++ src/app/components/host-card/host-card.component.ts
@@ -7,11 +7,13 @@ import { HostService } from '../../services/host.service';
 import { IHostOffer } from '../../interfaces/host';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { CardOfferComponent } from "../card-offer/card-offer.component";
+import { ExampleCardEComponent } from '../example-card-e/example-card-e.component';
 
 @Component({
   selector: 'app-host-card',
   standalone: true,
-  imports: [AsyncPipe, ExampleCardAComponent, ExampleCardBComponent, ExampleCardCComponent, ExampleCardDComponent],
+  imports: [AsyncPipe, ExampleCardAComponent, ExampleCardBComponent, ExampleCardCComponent, ExampleCardDComponent, ExampleCardEComponent],
   templateUrl: './host-card.component.html',
   styleUrl: './host-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
